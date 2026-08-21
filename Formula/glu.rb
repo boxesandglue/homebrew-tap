277ce852280a8b24f6360939ebfb2610d3e27888
Patrick Gundlach <gundlach@speedata.de>
2026-08-21 11:00:30 +0200

    Update glu formula for version 0.0.29

--- Formula/glu.rb
+++ Formula/glu.rb
@@ -1,26 +1,26 @@
 class Glu < Formula
   desc "Lua-based PDF creation tool using boxes and glue"
   homepage "https://github.com/boxesandglue/glu"
-  version "0.0.28"
+  version "0.0.29"
   license "AGPL-3.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/boxesandglue/glu/releases/download/v#{version}/glu-macos-arm64.tar.gz"
-      sha256 "78bc89939d934357bf0d5404f684277666c2e3ed969486ed31cc0cdb3904dc87"
+      sha256 "a4fe3d83a35c1d9524316cb792dcf9edc71b03e8fc478d36f0e49acfc3c18d10"
     else
       url "https://github.com/boxesandglue/glu/releases/download/v#{version}/glu-macos-amd64.tar.gz"
-      sha256 "a38c7b523eb0b04dc1b7ab080b54f12e363ec30c0434a8367822c06205d6e212"
+      sha256 "ffe76a0f58f610302eb032a0ba6cb61a0476bba6ecf22ab7b9bce99068c29bc0"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/boxesandglue/glu/releases/download/v#{version}/glu-linux-arm64.tar.gz"
-      sha256 "c6b53474cc326d0aa4bf422b28ea2efd7937677ce1f731664b0c03775398b817"
+      sha256 "ad4705418476aa6c28126a140c628d2f193e82767f8c0b6f86d7f46e5f4d0cac"
     else
       url "https://github.com/boxesandglue/glu/releases/download/v#{version}/glu-linux-amd64.tar.gz"
-      sha256 "915196491c53d561b8458df849b8b7a289c4933a1cbd6424522b86983bf70694"
+      sha256 "dfd8711ea3732b39c26d75a7b2198b58ac8e029a90863edc890f243978823f1b"
     end
   end
 
