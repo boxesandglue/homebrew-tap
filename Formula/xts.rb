f62df1e51b7f63d92044121814f2be6e1a369d44
Patrick Gundlach <gundlach@speedata.de>
2026-09-14 14:02:15 +0200

    Update xts formula for version 0.0.29

--- Formula/xts.rb
+++ Formula/xts.rb
@@ -1,26 +1,26 @@
 class Xts < Formula
   desc "XML-based PDF typesetting system"
   homepage "https://github.com/speedata/xts"
-  version "0.0.27"
+  version "0.0.29"
   license "AGPL-3.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/speedata/xts/releases/download/v#{version}/xts-macos-arm64.zip"
-      sha256 "0adba9efdb6787d7d8a190faddcdf82689c0670ca58966cc70f28a34495785c7"
+      sha256 "783ed1c8863d294c6886eb8978f8286be24e653eba7cd7e7e26e316e856af0eb"
     else
       url "https://github.com/speedata/xts/releases/download/v#{version}/xts-macos-amd64.zip"
-      sha256 "2eda24bd5260751830de025e098598bf9c9b7f39f5c135a07f33339efdcc4ec2"
+      sha256 "2fa95aac9f3936cb42e8763f5e87151e990aa3846f4ec382b55963bf6d35367c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/speedata/xts/releases/download/v#{version}/xts-linux-arm64.tar.gz"
-      sha256 "b58d8714558a6ca0844d498f736dbf0671f311e97fceeb928ab66df0e153eb92"
+      sha256 "de1649da36e09d9dc17872f4d7f64234b8ba39c7fe9b137451d4d26cf00872e5"
     else
       url "https://github.com/speedata/xts/releases/download/v#{version}/xts-linux-amd64.tar.gz"
-      sha256 "557a4e74d698cf6d0b67438538eb664a2a54b2df28b02b01651e75c14bb462db"
+      sha256 "ab28f119e4f79daf0234f156b8411a03cf77263eddaeeaa253afeee4a70ab8ae"
     end
   end
 
