c25a0fc9678651b126831453416075c967d194e3
Patrick Gundlach <gundlach@speedata.de>
2026-09-14 14:03:33 +0200

    Update goxslt formula for version 0.0.4

--- Formula/goxslt.rb
+++ Formula/goxslt.rb
@@ -1,26 +1,26 @@
 class Goxslt < Formula
   desc "XSLT 3.0 processor written in Go"
   homepage "https://github.com/speedata/goxslt"
-  version "0.0.4"
+  version "0.0.5"
   license "BSD-3-Clause"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/speedata/goxslt/releases/download/v#{version}/goxslt-macos-arm64.tar.gz"
-      sha256 "44aab84611ad31048d82ef7a17fd00cc29cf7cc436a10283cc20a8f1ccfdc6bd"
+      sha256 "fba7919e8d0afe5996ce5242d00114cf074c6c119d9d042e2f2758d154d531cb"
     else
       url "https://github.com/speedata/goxslt/releases/download/v#{version}/goxslt-macos-amd64.tar.gz"
-      sha256 "5c23da1e5b902039e2f40f72036abd5ac6ea694469c2eeffb0775ced9f2f4232"
+      sha256 "6bf61373a0d67dd965c55fec418c37bbf9d65dfeafa5e6f560d6ef97c9122c44"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/speedata/goxslt/releases/download/v#{version}/goxslt-linux-arm64.tar.gz"
-      sha256 "8da7fc6a46e5d7653447a9fad8f43af82b37f52d7bd899a2b75c7d9ceabd07d3"
+      sha256 "19c6c7b16df902d82facc9cf8cbeedb011f8ec97c54cce94314a5c454f1c9fe3"
     else
       url "https://github.com/speedata/goxslt/releases/download/v#{version}/goxslt-linux-amd64.tar.gz"
-      sha256 "6f0c61d4ff72be3e700421be864a8c60f23208516dfaaac072754968e5c1f35c"
+      sha256 "d10faf8a1eda58977af20a29fee4b673916a43639ac301785dda8acbe2f489f9"
     end
   end
 
